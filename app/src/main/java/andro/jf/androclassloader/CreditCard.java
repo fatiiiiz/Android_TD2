c85fcd4f30e54aa2283b7227c55f6844b4bd0505
unknown <agourram.fatimazahra@gmail.com>
2021-02-05 17:47:17 +0100

    Android TD2

---
+++ app/src/main/java/andro/jf/androclassloader/CreditCard.java
@@ -0,0 +1,18 @@
+package andro.jf.androclassloader;
+
+import java.io.Serializable;
+
+public class CreditCard implements Serializable {
+
+	// This is very sensitive !
+	private int number = 0;
+	
+	public CreditCard(int i) {
+		number = i;
+	}
+	
+	public int getNumber()
+	{
+		return number;
+	}
+}
